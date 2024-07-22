476f5c2336397616367c98e5fa2b85c5d472a253
sebteouii <sebteouiikhoubayb@gmail.com>
2024-07-22 17:12:33 +0200

    Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,9 +4,6 @@
 
 def index_range(page, page_size):
     """Return the tuple of start and end indices"""
-   
     start_index = (page - 1) * page_size
-  
     end_index = start_index + page_size
-    
     return (start_index, end_index)
