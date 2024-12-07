043bb5cf350881208f9040efbe331be8fedce18d
sebteouii <sebteouiikhoubayb@gmail.com>
2024-12-07 15:49:16 +0100

    sql alqemy declaration

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,28 +1,16 @@
+
 #!/usr/bin/env python3
-"""User Class File"""
+""" User model """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 Base = declarative_base()
 
 
 class User(Base):
-    """
-    -----------
-    CLASS: User
-    -----------
-    Description:
-        Creates a SQLAlchemy model for the users table.
-    """
+    """ SQLAlchemy model for a database table named users """
     __tablename__ = 'users'
-    __table_args__ = {'comment': 'Table storing user data for authentication'}
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(255), nullable=False, unique=True)
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255), nullable=True)
-    reset_token = Column(String(255), nullable=True)
-
-    def __repr__(self):
-        """String representation of the User object."""
-        return f"<User(id={self.id}, email={self.email})>"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
