67db87936e553dc73da7151f868838a4e9a88772
sebteouii <sebteouiikhoubayb@gmail.com>
2024-12-07 15:44:05 +0100

    sql alqemy declaration

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """User Class File"""
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 Base = declarative_base()
 
 
@@ -13,16 +11,12 @@ class User(Base):
     CLASS: User
     -----------
     Description:
-        Creates a SQLAlchemy model for the users table.
+        Creates a User SQLAchemy object
     """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(255), nullable=False, unique=True)  # Unique emails
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255), nullable=True)
-    reset_token = Column(String(255), nullable=True)
-
-    def __repr__(self):
-        """String representation of the User object."""
-        return f"<User(id={self.id}, email={self.email})>"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
