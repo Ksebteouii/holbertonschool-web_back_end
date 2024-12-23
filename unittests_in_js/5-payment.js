20a7991c79df2d4ad15ae94cd851e7a4f4726c9e
sebteouii <sebteouiikhoubayb@gmail.com>
2024-12-23 19:20:28 +0100

    Spies

---
+++ unittests_in_js/5-payment.js
@@ -0,0 +1,10 @@
+
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const amtPlusShipping = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${amtPlusShipping}`);
+  return amtPlusShipping;
+};
+
+module.exports = sendPaymentRequestToApi;
